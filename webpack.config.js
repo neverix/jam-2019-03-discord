eaa87e1fdf8070652dfe99cb511a3eb3d41670a5
neverix <stomperhomp@gmail.com>
2019-03-01 16:56:12 +0700

    basic webpack setup

---
+++ webpack.config.js
@@ -0,0 +1,46 @@
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+module.exports = {
+    devtool: 'inline-source-map',
+    module: {
+        rules: [
+            {
+                test: /\.ts$/,
+                use: 'ts-loader',
+                exclude: /node_modules/
+            },
+            {
+                test: /\.html$/,
+                use: [
+                    {
+                        loader: "html-loader"
+                    }
+                ]
+            },
+            {
+                test: /\.(png|mp3)$/,
+                use: [
+                    {
+                        loader: 'file-loader',
+                        options: {}
+                    }
+                ]
+            }
+        ]
+    },
+    plugins: [
+        new HtmlWebPackPlugin({
+            template: "./src/index.html",
+            filename: "./index.html"
+        })
+    ],
+    resolve: {
+        extensions: [
+            ".js",
+            ".ts"
+        ]
+    },
+    entry: [
+        'core-js',
+        "./src/index.ts"
+    ]
+};
