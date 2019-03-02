f5dfb6267ab861f16f40756a3b8beb233156c0ee
neverix <stomperhomp@gmail.com>
2019-03-02 19:35:28 +0700

    added text box

--- src/ts/main.ts
+++ src/ts/main.ts
@@ -1,11 +1,25 @@
 import { sceneTransition, addOnSceneTransition, getScene } from "./scenes"
 import { start } from "./game"
+import { typeText, hideTextbox } from "./textbox"
 
 sceneTransition("menu")
 addOnSceneTransition("game", () => {
     start(getScene("game"))
-    setTimeout(() => sceneTransition("menu"), 10000)
+    setTimeout(() => sceneTransition("menu"), 6000)
 })
 
+// add the sceneTransition() function to the window object so that it's accessible to outside scripts
 // @ts-ignore black magic
 window.sceneTransition = sceneTransition
+
+addOnSceneTransition("intro", () => {
+    typeText(`yay\nhello world\nyoyoyoyoyooyyoyoyoyooyoyoyoyoyoyoy`, [
+        {
+            text: "OK",
+            onClick: () => {
+                sceneTransition("game")
+                hideTextbox()
+            }
+        }
+    ])
+})
---
+++ src/ts/textbox.ts
@@ -0,0 +1,88 @@
+import { html, render } from "lit-html"
+import { unsafeHTML } from 'lit-html/directives/unsafe-html.js'
+
+// delay between typed characters
+const typerDelay = 50
+
+// the text currently being typed
+let sourceText = ""
+// the letter currently being typed
+let currentLetter = 0
+// the text in the textbox
+let textboxText = ""
+// id of the typer interval
+let typerId: number
+// type of the buttons
+type Buttons = { text: string, onClick: () => void }[]
+
+// the textbox container
+let textboxDiv = document.getElementById("textbox-div")
+
+// hides the textbox and hides it
+function hideTextbox() {
+    textboxDiv.style.display = "none"
+}
+hideTextbox()
+
+const okButton = { text: "OK", onClick: () => hideTextbox() }
+
+//start typing some text
+function typeText(
+    text: string,
+    buttons: Buttons = [okButton]) {
+    // clear the text
+    textboxText = ''
+    // reset the current letter
+    currentLetter = 0
+    // show textbox
+    textboxDiv.style.display = "block"
+    // set the target text
+    sourceText = text
+    // remove the typer if it exists
+    if (typerId) {
+        clearInterval(typerId)
+    }
+    // create a new typer
+    typerId = setInterval(() => typer(buttons), typerDelay)
+}
+
+// the typer types text to the textbox
+function typer(buttons: Buttons) {
+    // check if the entire text has been written
+    if (currentLetter >= sourceText.length) {
+        // stop the typer
+        clearInterval(typerId)
+    } else {
+        // copy the current letter from the source to the textbox
+        let char = sourceText[currentLetter]
+        textboxText += char
+        // move the current letter
+        currentLetter++
+        // render the text
+        renderText(textboxText, buttons)
+    }
+}
+
+// renders the current text to the textbox
+function renderText(textSoFar: string, buttons: Buttons) {
+    render(
+
+        html`
+                <!-- the actual text -->
+                <p>
+                    ${unsafeHTML(textSoFar.split("\n").join("<br>"))}
+                </p>
+                <!-- buttons -->
+                <!-- check if the text is completed -->
+                ${textSoFar.length < sourceText.length ? "" : buttons.map(button => html`
+                    <!-- create a button if it is -->
+                    <a class=textbox-button href=# @click=${button.onClick}>
+                        <!-- display the button text -->
+                        ${button.text}
+                    </a>`)
+                }
+            `
+        , textboxDiv)
+}
+
+export { typeText, hideTextbox }
